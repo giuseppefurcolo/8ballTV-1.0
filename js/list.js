48cc4d44a717ff2ff124fe0a2b1127335dde939e
G <me@giuseppefurcolo.com>
2021-08-26 13:00:41 -0400

    Update list.js

--- js/list.js
+++ js/list.js
@@ -5,5 +5,6 @@ var SEP= {file:"", title:"<hr>"};
 content.push( SEP );
 
 var protest_15 = {file:"15-protest1.mp4", title:"Protest in New York"}; content.push(protest_15);
+var giuseppe_15 = {file:"15-protest1.mp4", title:"Giuseppe first video"}; content.push(giuseppe_15);
 
 content.push( SEP );
